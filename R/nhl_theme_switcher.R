994bbf571cc45535f9af132bfd91a78b389ea800
Jason <jason@themechanicalbear.com>
2019-04-08 12:12:33 -0700

    sysdata.rda call

--- R/nhl_theme_switcher.R
+++ R/nhl_theme_switcher.R
@@ -28,7 +28,8 @@ nhl_theme_switcher <- function() {
 
     # team_colors <- readxl::read_excel("inst/extdata/team_colors.xlsx")
     # usethis::use_data(team_colors, internal = TRUE)
-    load("R/sysdata.rda")
+    # load("R/sysdata.rda")
+    team_colors
 
     # Listen for 'done' events. When we're finished, we'll update the theme, and then stop the gadget.
     observeEvent(input$done, {
